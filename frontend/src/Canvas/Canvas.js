d16bb068af54740474c55efa43347522056ba68a
iwaduarte <iwaduarte@gmail.com>
2020-03-23 21:48:09 -0300

    Remove styled component to use raw one.
    - styled component it was re-rendering and flickering background (bug?)

--- frontend/src/Canvas/Canvas.js
+++ frontend/src/Canvas/Canvas.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const Background = styled.div`
-    display: flex;
-    background-image: url("/images/canvas_bg.gif");
-    background-repeat: no-repeat;
-    background-position: center;
-    height: 95vh;
-    
-`;
+//using styled components makes the gif image to be flickering when an update occurred (flickers on render)
+// const Background = styled.div`
+//     display: flex;
+//     background-image: url("/images/canvas_bg.gif");
+//     background-repeat: no-repeat;
+//     background-position: center;
+//     height: 95vh;
+//
+// `;
+// const Canvas = () => <Background/>;
 
-const Canvas = ({children}) => <Background>{children}</Background>;
+const Canvas = ({children}) => <div style={{
+    display: "flex",
+    backgroundImage: `url("/images/canvas_bg.gif")`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "1000px 1000px",
+    width: "100vw",
+    position: "absolute",
+    zIndex: "-1",
+    height: "95vh"
+}}>{children}</div>;
+
 
 
 export default Canvas;
