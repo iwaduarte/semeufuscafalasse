b453474156905d0f36f2d3f805bbee5e642fe4ee
iwaduarte <iwaduarte@gmail.com>
2020-03-23 21:44:25 -0300

    App.js new states and configs
    - Player using forward Ref to manage positioning
    - Obstacle using forward Ref to manage positioning
    - new Obstacle configurations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,60 @@
-import React, {useState} from 'react';
+import React, {createRef, useState} from 'react';
 import Canvas from "./Canvas/Canvas";
 import Player from "./Player/Player"
 import Menu from "./Menu/Menu";
 import Interface from "./Interface/Interface";
 import useMove from "./Player/useMove";
 import playerConfig from "./Player/playerConfig";
+import obstacleConfig from "./Obstacles/obstaclesConfig";
+import Obstacles from "./Obstacles/Obstacles";
 
 const App = () => {
-    {/*// <Initialize/>*/
-    }
-    // console.log(display);
-    const [playerPosition, movePlayer] = useMove(playerConfig.LEFT);
+    //all repeated tasks across the game
+    const [intervalsId, setIntervalsId] = useState([]);
+    const [
+        playerPosition,
+        movePlayer,
+        obstaclePosition,
+        moveObstacle ] = useMove(playerConfig.LEFT, obstacleConfig, setIntervalsId);
     const [start, setStart] = useState(false);
-    const [initialize, setInitialize] = useState(false);
+    const [initialize, setInitialize] = useState(true);
+    const playerRef = createRef();
+    const obstacleRef = createRef();
 
     return <>
-        <Canvas>
-            {initialize ?
-                <Interface playerPosition={playerPosition}
-                           start={start}
-                           setStart={setStart}
-                />
-                : < Menu setInitialize={setInitialize}/>
+        {initialize ?
+            <Interface playerPosition={playerPosition}
+                       obstaclePosition={obstaclePosition}
+                       start={start}
+                       setStart={setStart}
+                       playerRef={playerRef}
+                       obstacleRef={obstacleRef}
+                       setIntervalsId={setIntervalsId}
+                       intervalsId={intervalsId}
+                       moveObstacle={moveObstacle}
+                       movePlayer={movePlayer}
+            />
+            : < Menu setInitialize={setInitialize}/>
 
-            }
-            {start && <Player movePlayer={movePlayer}
-                              playerPosition={playerPosition}
-                              display={start}/>
+        }
+        <Canvas>
+            {start && <>
+                <Player ref={playerRef}
+                        movePlayer={movePlayer}
+                        playerPosition={playerPosition}
+                        playerTop={playerConfig.TOP}
+                        display={start}/>
+                <Obstacles
+                    ref={obstacleRef}
+                    top={obstaclePosition.top}
+                    left={obstaclePosition.left}
+                    height={obstaclePosition.height}
+                    moveObstacle={moveObstacle}
+                />
+            </>
             }
         </Canvas>
-
     </>
 };
-
 
 export default App;
