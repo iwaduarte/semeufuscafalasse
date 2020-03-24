a509f0285fc64778566dba48380db0021994061b
iwaduarte <iwaduarte@gmail.com>
2020-03-23 21:49:14 -0300

    Obstacle sprite.
    - rocks.jpg, grass.jpgs, etc will have a move function to give a challenge to player.

---
+++ frontend/src/Obstacles/Obstacles.js
@@ -0,0 +1,37 @@
+import React, {forwardRef, useEffect} from 'react';
+import styled from 'styled-components';
+
+const obstacleSrc = "/images/obstacles/rock.png";
+const ObstacleSprite = styled.img`
+    // display: ${props => props.display || 'none'};
+    position: absolute;
+    z-index: 2;
+    top: ${props => props.top && `${props.top}%`};
+    left: ${props => props.left && `${props.left}%`};
+    height: ${props => props.propsHeight ? `${props.propsHeight}px` : '33px'};
+    box-sizing: border-box;
+    // border: 2px solid red;
+`;
+
+
+const Obstacles = forwardRef( ({top, left, height, moveObstacle}, ref) => {
+
+    // const ref = useRef(null);
+    //move Obstacle when rendered
+    useEffect(() => {
+        // console.log('once');
+        moveObstacle();
+
+    }, [moveObstacle]);
+
+    return <>
+        <ObstacleSprite ref={ref}
+                        top={top}
+                        left={left}
+                        propsHeight={height}
+                        src={obstacleSrc}/>
+
+    </>
+});
+
+export default Obstacles;
---
+++ frontend/src/Obstacles/obstaclesConfig.js
@@ -0,0 +1,9 @@
+//related to initial configs
+const config = {
+    left: 42,
+    top: 48,
+    height: 33,
+    width: 33,
+};
+
+export default config;
