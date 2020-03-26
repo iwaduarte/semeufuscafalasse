c7f0cc5d510b6537e7f92b1a96c80177c6bf225e
iwaduarte <iwaduarte@gmail.com>
2020-03-25 21:56:58 -0300

    Added GraphQL Apollo Client with Hooks
    - removed react logo image
    - added Effects to manage restart and reset states
    - random Obstacles added
    - better management of async functions inside useEffect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,107 @@
-import React, {createRef, useState} from 'react';
+import React, {createRef, useEffect, useState} from 'react';
 import Canvas from "./Canvas/Canvas";
 import Player from "./Player/Player"
 import Menu from "./Menu/Menu";
 import Interface from "./Interface/Interface";
-import useMove from "./Player/useMove";
+import useMove from "./myHooks/useMove";
 import playerConfig from "./Player/playerConfig";
 import obstacleConfig from "./Obstacles/obstaclesConfig";
+
 import Obstacles from "./Obstacles/Obstacles";
 
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from '@apollo/react-hooks';
+//remove after setting proxy dev.
+const graphQLconfig = {
+    uri: "http://localhost:3005/graphql",
+    onError: ({networkError, graphQLErrors}) => {
+        console.log('graphQLErrors', graphQLErrors);
+        console.log('networkError', networkError);
+    }
+};
+const client = new ApolloClient(graphQLconfig);
+//random start positions
+const randomObstacleConfig = obstacleConfig[Math.floor(Math.random() * 3)];
+
 const App = () => {
-    //all repeated tasks across the game
+
+    //all repeated set Interval tasks across the game
     const [intervalsId, setIntervalsId] = useState([]);
+    const [playerName, setPlayerName] = useState('');
+    const [playerEmail, setPlayerEmail] = useState('');
     const [
-        playerPosition,
-        movePlayer,
-        obstaclePosition,
-        moveObstacle ] = useMove(playerConfig.LEFT, obstacleConfig, setIntervalsId);
+        playerPosition, movePlayer,
+        obstaclePosition, moveObstacle,
+        resetPositions] = useMove(playerConfig, randomObstacleConfig, setIntervalsId);
     const [start, setStart] = useState(false);
-    const [initialize, setInitialize] = useState(true);
+    const [initialize, setInitialize] = useState(false);
+    const [resetGame, setResetGame] = useState(false);
+    const [resetAll, setResetAll] = useState(false);
     const playerRef = createRef();
     const obstacleRef = createRef();
 
-    return <>
-        {initialize ?
-            <Interface playerPosition={playerPosition}
-                       obstaclePosition={obstaclePosition}
-                       start={start}
-                       setStart={setStart}
-                       playerRef={playerRef}
-                       obstacleRef={obstacleRef}
-                       setIntervalsId={setIntervalsId}
-                       intervalsId={intervalsId}
-                       moveObstacle={moveObstacle}
-                       movePlayer={movePlayer}
-            />
-            : < Menu setInitialize={setInitialize}/>
+    //reset game to main meu
+    useEffect(() => {
+        if (resetAll) {
+            resetPositions();
+            setStart(false);
+            setInitialize(false);
+            setPlayerName('');
+            setPlayerEmail('');
 
         }
-        <Canvas>
-            {start && <>
-                <Player ref={playerRef}
-                        movePlayer={movePlayer}
-                        playerPosition={playerPosition}
-                        playerTop={playerConfig.TOP}
-                        display={start}/>
-                <Obstacles
-                    ref={obstacleRef}
-                    top={obstaclePosition.top}
-                    left={obstaclePosition.left}
-                    height={obstaclePosition.height}
-                    moveObstacle={moveObstacle}
-                />
-            </>
-            }
-        </Canvas>
+
+    }, [resetAll]);
+
+    //restart match
+    useEffect(() => {
+        if (resetGame) {
+            resetPositions();
+            setStart(false);
+            setInitialize(true);
+            setResetGame(false);
+        }
+
+    }, [resetGame]);
+
+    return <>
+        <ApolloProvider client={client}>
+
+            {!initialize && < Menu playerName={playerName}
+                                   setPlayerName={setPlayerName}
+                                   playerEmail={playerEmail}
+                                   setPlayerEmail={setPlayerEmail}
+                                   setInitialize={setInitialize}
+                                   setResetAll = {setResetAll}
+            />}
+            <Canvas>
+                {initialize && <Interface playerName={playerName} playerEmail={playerEmail}
+                                          resetAll={resetAll} setResetAll={setResetAll}
+                                          resetGame={resetGame} setResetGame={setResetGame}
+                                          start={start} setStart={setStart}
+                                          setInitialize={setInitialize}
+                                          playerRef={playerRef} obstacleRef={obstacleRef}
+                                          intervalsId={intervalsId} setIntervalsId={setIntervalsId}
+                                          moveObstacle={moveObstacle} movePlayer={movePlayer}
+                />}
+                {start && <>
+                    <Player ref={playerRef}
+                            movePlayer={movePlayer}
+                            playerPosition={playerPosition}
+                            playerTop={playerConfig.TOP}
+                            display={start}/>
+                    <Obstacles
+                        ref={obstacleRef}
+                        top={obstaclePosition.top}
+                        left={obstaclePosition.left}
+                        height={obstaclePosition.height}
+                        moveObstacle={moveObstacle}
+                    />
+                </>
+                }
+            </Canvas>
+
+        </ApolloProvider>
     </>
 };
 
