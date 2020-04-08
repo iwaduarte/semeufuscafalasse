225635acbfe7638b7acb8658e57e89ba0142723a
iwaduarte <iwaduarte@gmail.com>
2020-04-08 18:22:44 -0300

    Add timer function and remove comments

--- frontend/src/Interface/Engine.js
+++ frontend/src/Interface/Engine.js
@@ -1,23 +1,35 @@
 
-// start points either creating it or receiving an initial value
-// use a callback fn provided (setPoints) and return a set Interval for executing it.
-const startPoints = (setPoints, points) => {
-    if (!setPoints) return;
+const startPoints = (setPointsFn, points) => {
+    if (!setPointsFn) return;
     const startPoint = points ? Date.now() - (points * 100) : Date.now();
-    if (typeof setPoints !== 'function') return;
+    if (typeof setPointsFn !== 'function') return;
     return setInterval(() => {
         const points = Date.now() - startPoint;
-        setPoints(Math.floor(points / 100));
+        setPointsFn(Math.floor(points / 100));
     }, 50);
 
 };
 
-//check collision between two sprites and consider a percentage of how much collision is considering a collision
-// (i.e a simple touch (square lines touch it) or a deep collision area
+const timer = (seconds, setSeconds, setIntervalsId) => {
+    const timer = seconds * 1000;
+    const dateNow = Date.now();
+    return new Promise((resolve, reject) => {
+        const intervalId = setInterval(() => {
+            const elapsedTime = Date.now() - dateNow;
+            const elapsedTimeCountdown = seconds - Math.floor(elapsedTime / 1000);
+            setSeconds(elapsedTimeCountdown);
+            if (elapsedTime >= timer) {
+                resolve(true);
+            }
+        }, 50);
+        setIntervalsId(prevState => [...prevState, intervalId]);
+
+    });
+};
+
 const checkCollision = (spriteA, spriteB, percentageCollision) => {
     if (!spriteA || !spriteB) return false;
     //mdn collision algorithm see more at https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-    //algorithm edited to consider unusual shape and collided only when more noticeable;
     const percentageExtra = percentageCollision || 1;
     const collision = spriteA.x * percentageExtra < spriteB.x + spriteB.width &&
         (spriteA.x  + spriteA.width ) * percentageExtra > spriteB.x &&
@@ -31,5 +43,4 @@ const checkCollision = (spriteA, spriteB, percentageCollision) => {
     return false;
 };
 
-
-export {startPoints, checkCollision};
+export {startPoints, checkCollision, timer};
