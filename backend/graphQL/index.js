c205d318a66f93106a31dc05c60b19daf00baa6d
Iwá Duarte <iwaduarte@gmail.com>
2022-10-17 20:43:12 -0300

    fix: graphql middleware

--- backend/graphQL/index.js
+++ backend/graphQL/index.js
@@ -1,21 +1,13 @@
-module.exports = (app) => {
-    const {ApolloServer, gql} = require('apollo-server-express');
-    const {typeDefs, resolvers} = require('./schema');
+const { ApolloServer, gql } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schema");
 
-    const server = new ApolloServer({
-        cors: {
-            origin: '*',
-            credentials: true,
-        },
-        typeDefs,
-        resolvers,
-    });
+const GQLServer = new ApolloServer({
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+  typeDefs,
+  resolvers,
+});
 
-    server.applyMiddleware({app});
-
-    return server.graphqlPath;
-};
-
-// app.listen({ port: 4000 }, () =>
-//     console.log(`🚀 GraphQL ready at http://localhost:4000${server.graphqlPath}`)
-// )
+module.exports = { GQLServer };
