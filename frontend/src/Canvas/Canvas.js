2a80759893c7991df64cb268e896a81057b6b4a4
Iwá Duarte <iwaduarte@gmail.com>
2023-04-22 21:31:17 -0300

    feat: update canvas

--- frontend/src/Canvas/Canvas.js
+++ frontend/src/Canvas/Canvas.js
@@ -1,32 +1,40 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
-
-const Background = styled.div.attrs(({ canvasBg }) => ({
-  style: {
-    backgroundImage: `url(${canvasBg})`
-  }
-}))`
-  display: flex;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: 1000px 1000px;
-  width: 1000px;
-  margin: auto;
-  position: absolute;
-  z-index: -1;
-  height: 95vh;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-`;
+import React, { useEffect, useRef } from 'react';
 
 const Canvas = ({ children, canvasBg }) => {
+  const canvasRef = useRef(null);
+
   useEffect(() => {
-    console.log(`CanvasBg`, canvasBg);
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    const img = new Image();
+    img.src = canvasBg;
+    img.onload = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(img, 0, 0, 1000, 1000);
+    };
+
+    console.log('[Canvas] useEffect: canvasBg', canvasBg);
   }, [canvasBg]);
 
-  return <canvas>{children}</canvas>;
+  const containerStyle = {
+    display: 'flex',
+    width: '1000px',
+    margin: 'auto',
+    position: 'absolute',
+    zIndex: -1,
+    height: '95vh',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  };
+
+  return (
+    <div style={containerStyle}>
+      <canvas ref={canvasRef} width="1000" height="1000"></canvas>
+      {children}
+    </div>
+  );
 };
 
 export default Canvas;
