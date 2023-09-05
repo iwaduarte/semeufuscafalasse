13a4c9c278b939649ae49e4509fa12e70849b062
iwaduarte <iwaduarte@gmail.com>
2023-09-05 15:27:40 -0300

    feat: fixing conf vite migration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: './dist/frontend'
-  },
-  server: {
-    port: 4200,
-    open: true,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.js',
-    css: true,
-  },
   plugins: [react()],
 });
